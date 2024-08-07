f96bb6c2ceadacfc2b30e388709a2ab4f005826f
[Carlos Loaiza] <[cloaiza928@gmail.com]>
2024-08-06 20:52:34 -0500

    Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./layaut/footer/footer.component";
+import { HeaderComponent } from "./layaut/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/signal/signal-ejemplo2/signal-ejemplo2.component.ts
@@ -0,0 +1,19 @@
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-ejemplo2',
+  standalone: true,
+  imports: [],
+  templateUrl: './signal-ejemplo2.component.html',
+  styleUrls: ['./signal-ejemplo2.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class SignalEjemplo2Component {
+
+  contador= signal(0);
+
+  constructor(){
+    setInterval(()=> this.contador.set(this.contador() + 1), 1000)
+  }
+
+}
